dfc5efac75d236439d9164f434dc99631d408bdb
nico.guanziroli <nico.guanziroli@accenture.com>
2026-05-01 09:48:29 +0100

    Added main menu webpage, previous bookings webpage and styled css.

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
-
 def index():
     return render_template('index.html')
+
+@app.route("/mainmenu")
+def mainmenu():
+    return render_template("mainmenu.html")
+
+@app.route("/prevbookings")
+def prevbookings():
+    return render_template("prevbookings.html")
 
 @app.route('/button-clicked', methods=['POST'])
 def button_clicked():
